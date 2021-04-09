d9194ddc9a4ea5123a4e135e893ce4516545655e
Michael Ho <callmemikeh@gmail.com>
2021-04-09 11:45:12 -0700

    Convert indents to spaces
    Unfortunately, I'm still struggling with my editor, so I may need to do
    this again.

--- internal/bazel/label.go
+++ internal/bazel/label.go
@@ -8,105 +8,105 @@ import (
 )
 
 var (
-	labelRegexp = regexp.MustCompile(`^//([\w/]*)(?::(\w+))?$`)
-	relativeLabelRegexp = regexp.MustCompile(`^:(\w+)$`)
+  labelRegexp = regexp.MustCompile(`^//([\w/]*)(?::(\w+))?$`)
+  relativeLabelRegexp = regexp.MustCompile(`^:(\w+)$`)
 )
 
 func JoinLabelStrings(labels []*Label, separator string) string {
-	var out string
-	for i, label := range labels {
-		if i > 0 {
-			out += separator
-		}
-		out += label.String()
-	}
-	return out
+  var out string
+  for i, label := range labels {
+    if i > 0 {
+      out += separator
+    }
+    out += label.String()
+  }
+  return out
 }
 
 // NewLabel creates a new label.
 func NewLabel(absDir, name, workspaceDir string) (*Label, error) {
-	if !filepath.IsAbs(absDir) {
-		return nil, fmt.Errorf("%q must be an absolute path", absDir)
-	}
-	if !strings.HasPrefix(absDir, workspaceDir) {
-		return nil, fmt.Errorf("%q must be in %q", absDir, workspaceDir)
-	}
-	dir, err := filepath.Rel(workspaceDir, absDir)
-	if err != nil {
-		return nil, fmt.Errorf("filepath.Rel(%q, %q): %v", workspaceDir, absDir, err)
-	}
-	return &Label{
-		dir: dir,
-		name: name,
-	}, nil
+  if !filepath.IsAbs(absDir) {
+    return nil, fmt.Errorf("%q must be an absolute path", absDir)
+  }
+  if !strings.HasPrefix(absDir, workspaceDir) {
+    return nil, fmt.Errorf("%q must be in %q", absDir, workspaceDir)
+  }
+  dir, err := filepath.Rel(workspaceDir, absDir)
+  if err != nil {
+    return nil, fmt.Errorf("filepath.Rel(%q, %q): %v", workspaceDir, absDir, err)
+  }
+  return &Label{
+    dir: dir,
+    name: name,
+  }, nil
 }
 
 func ParseLabel(label string) (*Label, error) {
-	capture := labelRegexp.FindStringSubmatch(label)
-	if capture == nil {
-		return nil, fmt.Errorf("%q does not match %q", label, labelRegexp)
-	}
-	dir := capture[1]
-	name := capture[2]
-	if name == "" {
-		if dir == "" {
-			return nil, fmt.Errorf("%q returned an empty regexp capture", label)
-		}
-		name = filepath.Base(dir)
-	}
-	return &Label{
-		dir: dir,
-		name: name,
-	}, nil
+  capture := labelRegexp.FindStringSubmatch(label)
+  if capture == nil {
+    return nil, fmt.Errorf("%q does not match %q", label, labelRegexp)
+  }
+  dir := capture[1]
+  name := capture[2]
+  if name == "" {
+    if dir == "" {
+      return nil, fmt.Errorf("%q returned an empty regexp capture", label)
+    }
+    name = filepath.Base(dir)
+  }
+  return &Label{
+    dir: dir,
+    name: name,
+  }, nil
 }
 
 func ParseRelativeLabel(other *Label, label string) (*Label, error) {
-	if other == nil {
-		return nil, fmt.Errorf("other label is nil")
-	}
-	capture := relativeLabelRegexp.FindStringSubmatch(label)
-	if len(capture) == 0 {
-		return ParseLabel(label)
-	}
-	name := capture[1]
-	if name == "" {
-		return nil, fmt.Errorf("%q returned an empty regexp capture", label)
-	}	
-	return &Label{
-		dir: other.dir,
-		name: name,
-	}, nil
+  if other == nil {
+    return nil, fmt.Errorf("other label is nil")
+  }
+  capture := relativeLabelRegexp.FindStringSubmatch(label)
+  if len(capture) == 0 {
+    return ParseLabel(label)
+  }
+  name := capture[1]
+  if name == "" {
+    return nil, fmt.Errorf("%q returned an empty regexp capture", label)
+  }	
+  return &Label{
+    dir: other.dir,
+    name: name,
+  }, nil
 }
 
 // Label is a Bazel BUILD label.
 type Label struct {
-	// Relative dir from 
-	dir string
-	name string
+  // Relative dir from 
+  dir string
+  name string
 }
 
 // Name returns the label's name.
 func (l *Label) Name() string {
-	return l.name
+  return l.name
 }
 
 // Dir returns the directory the label belongs in.
 func (l *Label) Dir() string {
-	return l.dir
+  return l.dir
 }
 
 func (l *Label) String() string {
-	out := fmt.Sprintf("//%s", l.dir)
-	if filepath.Base(l.dir) != l.name {
-		out = fmt.Sprintf("%s:%s", out, l.name)
-	}
-	return out
+  out := fmt.Sprintf("//%s", l.dir)
+  if filepath.Base(l.dir) != l.name {
+    out = fmt.Sprintf("%s:%s", out, l.name)
+  }
+  return out
 }
 
 // RelativeTo generates the label string relative to another label.
 func (l *Label) RelativeTo(other *Label) string {
-	if l.dir != other.dir {
-		return l.String()
-	}
-	return fmt.Sprintf(":%s", l.name)
+  if l.dir != other.dir {
+    return l.String()
+  }
+  return fmt.Sprintf(":%s", l.name)
 }
--- nrfbazelify/output.go
+++ nrfbazelify/output.go
@@ -10,107 +10,107 @@ import (
 )
 
 func OutputBuildFiles(workspaceDir string, depGraph *DependencyGraph) error {
-	files := make(map[string]*buildfile.File)
+  files := make(map[string]*buildfile.File)
 
-	// Convert depGraph nodes into BUILD files.
-	nodes := depGraph.Nodes()
-	for _, node := range nodes {
-		c, err := extractBuildContents(node, depGraph)
-		if err != nil {
-			return err
-		}
-		if c == nil {
-			continue
-		}
-		if files[c.label.Dir()] == nil {
-			files[c.label.Dir()] = buildfile.New(filepath.Join(workspaceDir, c.label.Dir()))
-		}
-		files[c.label.Dir()].AddLibrary(c.library)
-	}
+  // Convert depGraph nodes into BUILD files.
+  nodes := depGraph.Nodes()
+  for _, node := range nodes {
+    c, err := extractBuildContents(node, depGraph)
+    if err != nil {
+      return err
+    }
+    if c == nil {
+      continue
+    }
+    if files[c.label.Dir()] == nil {
+      files[c.label.Dir()] = buildfile.New(filepath.Join(workspaceDir, c.label.Dir()))
+    }
+    files[c.label.Dir()].AddLibrary(c.library)
+  }
 
-	// Make sure we load cc_library in each BUILD file.
-	for _, file := range files {
-		file.AddLoad(&buildfile.Load{
-			Source: "@rules_cc//cc:defs.bzl",
-			Symbols: []string{"cc_library"},
-		})
-	}
+  // Make sure we load cc_library in each BUILD file.
+  for _, file := range files {
+    file.AddLoad(&buildfile.Load{
+      Source: "@rules_cc//cc:defs.bzl",
+      Symbols: []string{"cc_library"},
+    })
+  }
 
-	// Write BUILD file contents.
-	for _, file := range files {
-		if err := file.Write(); err != nil {
-			return err
-		}
-	}
+  // Write BUILD file contents.
+  for _, file := range files {
+    if err := file.Write(); err != nil {
+      return err
+    }
+  }
 
-	return nil
+  return nil
 }
 
 type buildContents struct {
-	label *bazel.Label
-	library *buildfile.Library
+  label *bazel.Label
+  library *buildfile.Library
 }
 
 func extractBuildContents(node Node, depGraph *DependencyGraph) (*buildContents, error) {
-	switch n := node.(type) {
-	case *LibraryNode:
-		return libraryContents(n, depGraph), nil
-	case *GroupNode:
-		return groupContents(n, depGraph), nil
-	case *OverrideNode:
-		// Override nodes are ignored, they just represent a label,
-		// and don't need any rules written.
-		return nil, nil
-	default:
-		return nil, fmt.Errorf("unknown node type for node %q", n.Label())
-	}
+  switch n := node.(type) {
+  case *LibraryNode:
+    return libraryContents(n, depGraph), nil
+  case *GroupNode:
+    return groupContents(n, depGraph), nil
+  case *OverrideNode:
+    // Override nodes are ignored, they just represent a label,
+    // and don't need any rules written.
+    return nil, nil
+  default:
+    return nil, fmt.Errorf("unknown node type for node %q", n.Label())
+  }
 }
 
 func libraryContents(node *LibraryNode, depGraph *DependencyGraph) *buildContents {
-	var deps []string
-	depNodes := depGraph.Dependencies(node.Label())
-	for _, d := range depNodes {
-		deps = append(deps, d.Label().RelativeTo(node.Label()))
-	}
+  var deps []string
+  depNodes := depGraph.Dependencies(node.Label())
+  for _, d := range depNodes {
+    deps = append(deps, d.Label().RelativeTo(node.Label()))
+  }
 
   // Sort the srcs, hdrs, and deps so output has a deterministic order.
   // This is especially useful for tests.
-	sort.Strings(node.Srcs)
-	sort.Strings(node.Hdrs)
-	sort.Strings(deps)
+  sort.Strings(node.Srcs)
+  sort.Strings(node.Hdrs)
+  sort.Strings(deps)
 
-	return &buildContents{
-		label: node.Label(),
-		library: &buildfile.Library{
-			Name: node.Label().Name(),
-			Srcs: node.Srcs,
-			Hdrs: node.Hdrs,
-			Deps: deps,
-			Includes: []string{"."},
-		},
-	}
+  return &buildContents{
+    label: node.Label(),
+    library: &buildfile.Library{
+      Name: node.Label().Name(),
+      Srcs: node.Srcs,
+      Hdrs: node.Hdrs,
+      Deps: deps,
+      Includes: []string{"."},
+    },
+  }
 }
 
 func groupContents(node *GroupNode, depGraph *DependencyGraph) *buildContents {
-	var deps []string
-	depNodes := depGraph.Dependencies(node.Label())
-	for _, d := range depNodes {
-		deps = append(deps, d.Label().RelativeTo(node.Label()))
-	}
+  var deps []string
+  depNodes := depGraph.Dependencies(node.Label())
+  for _, d := range depNodes {
+    deps = append(deps, d.Label().RelativeTo(node.Label()))
+  }
 
   // Sort the srcs, hdrs, and deps so output has a deterministic order.
   // This is especially useful for tests.
-	sort.Strings(node.Srcs)
-	sort.Strings(node.Hdrs)
-	sort.Strings(deps)
-	
-	return &buildContents{
-		label: node.Label(),
-		library: &buildfile.Library{
-			Name: node.Label().Name(),
-			Srcs: node.Srcs,
-			Hdrs: node.Hdrs,
-			Deps: deps,
-		},
-	}
+  sort.Strings(node.Srcs)
+  sort.Strings(node.Hdrs)
+  sort.Strings(deps)
+  
+  return &buildContents{
+    label: node.Label(),
+    library: &buildfile.Library{
+      Name: node.Label().Name(),
+      Srcs: node.Srcs,
+      Hdrs: node.Hdrs,
+      Deps: deps,
+    },
+  }
 }
