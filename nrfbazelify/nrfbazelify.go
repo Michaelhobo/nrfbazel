e921e7b5177fb539eab25e028ff1cb2b428bbb6d
Michael Ho <callmemikeh@gmail.com>
2021-04-21 02:17:27 -0700

    Move all output files to .bazelify-out in SDK dir
    Before, we had to specify paths for each DOT graph output.
    This was cumbersome and error-prone. Now, just write everything to
    .bazelify-out/dot, which allows us to mass-convert all DOT graphs to PNG

--- nrfbazelify/nrfbazelify.go
+++ nrfbazelify/nrfbazelify.go
@@ -8,14 +8,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 var (
-  dotGraphPath = flag.String("dot_graph_path", "", "The path to write the DOT graph. Omit to skip DOT graph output.")
-  dotGraphProgressionDir = flag.String("dot_graph_progression_dir", "", "The path to a directory to write the DOT graph progression. Omit to skip.")
-  namedGroupGraphsDir = flag.String("named_group_graphs_dir", "", "The path to a directory to write the DOT graphs of all named groups. Omit to skip.")
+  fullGraph = flag.Bool("full_graph", false, "Whether to create a DOT graph of the full graph.")
+  progressionGraphs = flag.Bool("progression_graphs", false, "Whether to create a DOT graph for each change in the graph.")
+  namedGroupGraphs = flag.Bool("named_group_graphs", false, "Whether to create a DOT graph for each named group.")
 )
 
 // GenerateBuildFiles generates BUILD files for an nRF5 SDK.
@@ -33,19 +34,50 @@ func GenerateBuildFiles(workspaceDir, sdkDir string, verbose bool) error {
   if err != nil {
     return fmt.Errorf("ReadBazelifyRC: %v", err)
   }
-  graph := NewDependencyGraph(conf, *dotGraphProgressionDir)
-  if *dotGraphPath != "" {
-    defer func(path string) {
-      log.Printf("Saving dependency graph to %s", path)
-      if err := graph.OutputDOTGraph(path); err != nil {
-        log.Printf("OutputDOTGraph(%q): %v", path, err)
-      }
-    }(*dotGraphPath)
+
+  // Setup .bazelify-out directory.
+  bazelifyOutDOTDir := filepath.Join(sdkDir, ".bazelify-out", "dot")
+
+  fullGraphDir := filepath.Join(bazelifyOutDOTDir, "full_graph")
+  progressionGraphsDir := filepath.Join(bazelifyOutDOTDir, "progression_graphs")
+  namedGroupGraphsDir := filepath.Join(bazelifyOutDOTDir, "named_group_graphs")
+
+  // Remove all outputs from .bazelify-out file.
+  for _, dir := range []string{fullGraphDir, progressionGraphsDir, namedGroupGraphsDir} {
+    if err := os.RemoveAll(dir); err != nil {
+      return fmt.Errorf("os.RemoveAll(%q): %v", dir, err)
+    }
   }
+
+  // Set up progression graph.
+  var progGraphDir string
+  if *progressionGraphs {
+    if err := os.MkdirAll(progressionGraphsDir, 0755); err != nil {
+      return fmt.Errorf("MkdirAll(%q): %v", progressionGraphsDir, err)
+    }
+    progGraphDir = progressionGraphsDir
+  }
+
+  graph := NewDependencyGraph(conf, progGraphDir)
+
+  // Set up output of the full DOT graph.
+  if *fullGraph {
+    if err := os.MkdirAll(fullGraphDir, 0755); err != nil {
+      return fmt.Errorf("MkdirAll(%q): %v", fullGraphDir, err)
+    }
+    defer func() {
+      log.Printf("Saving dependency graph to %s", fullGraphDir)
+      if err := graph.OutputDOTGraph(filepath.Join(fullGraphDir, "full_graph.dot")); err != nil {
+        log.Printf("OutputDOTGraph(%q): %v", fullGraphDir, err)
+      }
+    }()
+  }
+
   walker, err := NewSDKWalker(conf, graph)
   if err != nil {
     return fmt.Errorf("NewSDKWalker: %v", err)
   }
+
   unresolvedDeps, err := walker.PopulateGraph()
   if err != nil {
     return fmt.Errorf("SDKWalker.PopulateGraph: %v", err)
@@ -53,6 +85,7 @@ func GenerateBuildFiles(workspaceDir, sdkDir string, verbose bool) error {
   if len(unresolvedDeps) > 0 {
     return WriteUnresolvedDepsHint(conf, unresolvedDeps)
   }
+
   unnamedGroups, err := NameGroups(conf, graph)
   if err != nil {
     return fmt.Errorf("NameGroups: %v", err)
@@ -60,9 +93,11 @@ func GenerateBuildFiles(workspaceDir, sdkDir string, verbose bool) error {
   if len(unnamedGroups) > 0 {
     return WriteUnnamedGroupsHint(conf, unnamedGroups)
   }
+
   if err := OutputBuildFiles(conf, graph); err != nil {
     return fmt.Errorf("OutputBuildFiles: %v", err)
   }
+
   if err := RemoveStaleHint(sdkDir); err != nil {
     return fmt.Errorf("removeStaleHintFile: %v", err)
   }
@@ -72,10 +107,16 @@ func GenerateBuildFiles(workspaceDir, sdkDir string, verbose bool) error {
     return fmt.Errorf("NewGraphStats: %v", err)
   }
   log.Print(stats.GenerateReport())
-  if *namedGroupGraphsDir != "" {
-    if err := stats.WriteNamedGroupGraphs(*namedGroupGraphsDir); err != nil {
+
+  // Now that the graph is complete, write out all named groups for visualization.
+  if *namedGroupGraphs {
+    if err := os.MkdirAll(namedGroupGraphsDir, 0755); err != nil {
+      return fmt.Errorf("MkdirAll(%q): %v", namedGroupGraphsDir, err)
+    }
+    if err := stats.WriteNamedGroupGraphs(namedGroupGraphsDir); err != nil {
       return fmt.Errorf("WriteNamedGroupGraphs: %v", err)
     }
   }
+
   return nil
 }
