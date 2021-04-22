4e8c0e55332a027787a183fe30a868dbb7ad3405
Michael Ho <callmemikeh@gmail.com>
2021-04-21 21:29:58 -0700

    Move all output files to .bazelify-out in SDK dir (#61)
    Before, we had to specify paths for each DOT graph output.
    This was cumbersome and error-prone. Now, just write everything to
    .bazelify-out/dot, which allows us to mass-convert all DOT graphs to PNG

--- devtools/run_example.sh
+++ devtools/run_example.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+#!/bin/bash -e
 ROOT="$(realpath $(dirname $0)/..)"
 SDK="${ROOT}/example"
-DOT_GRAPH_PATH="${SDK}/depgraph.dot"
-DOT_GRAPH_PROGRESSION_DIR="${SDK}/depgraph_progression_dot"
-DOT_GRAPH_PROGRESSION_PNG_DIR="${SDK}/depgraph_progression"
-rm -rf "${DOT_GRAPH_PROGRESSION_DIR}"
-mkdir -p "${DOT_GRAPH_PROGRESSION_DIR}"
-rm -rf "${DOT_GRAPH_PROGRESSION_PNG_DIR}"
-mkdir -p "${DOT_GRAPH_PROGRESSION_PNG_DIR}"
-bazel run //cmd/nrfbazelify -- --workspace "${ROOT}" --sdk "${SDK}" --dot_graph_path "${DOT_GRAPH_PATH}" --dot_graph_progression_dir "${DOT_GRAPH_PROGRESSION_DIR}"
-dot -Tpng "${DOT_GRAPH_PATH}" > "${SDK}/depgraph.png"
-for f in ${DOT_GRAPH_PROGRESSION_DIR}/*
+bazel run //cmd/nrfbazelify -- \
+  --workspace "${ROOT}" --sdk "${SDK}" \
+  --full_graph --progression_graphs --named_group_graphs
+
+DOT_OUT=${SDK}/.bazelify-out/dot
+PNG_OUT=${SDK}/.bazelify-out/png
+mkdir -p "${PNG_OUT}"
+for f in ${DOT_OUT}/*/*.dot
 do
-  NAME="$(basename ${f} .dot)"
-  dot -Tpng "${f}" > "${DOT_GRAPH_PROGRESSION_PNG_DIR}/${NAME}.png"
+  OFFSET=${f#$DOT_OUT}
+  OFFSET=${OFFSET%dot}
+  DIR=$(dirname ${PNG_OUT}${OFFSET})
+  mkdir -p ${DIR}
+  dot -Tpng "${f}" > "${PNG_OUT}${OFFSET}png"
 done
